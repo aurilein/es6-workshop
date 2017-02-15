f6f26ef4c2b30f2fcda2a18adac3b955db31f010
aurilein <auriane@gmx.at>
2017-02-15 14:20:11 +0100

    Remove Hello example.

--- src/js/01/hello.js
+++
@@ -1,5 +0,0 @@
-function greet(name) {
-    return `Hello ${name}`;
-}
-
-document.getElementById("output").innerHTML = greet("World");
--- src/js/application.js
+++ src/js/application.js
@@ -1,6 +1,3 @@
-// 01
-require('./01/hello.js');
-
 // ticker
 const ticker_module = require('./ticker.js');
 const ticker = new ticker_module.Ticker({});
