976e42c63a3ad97f8a55c751494da954658c6a64
aurilein <auriane@gmx.at>
2017-02-15 14:05:08 +0100

    Set up Ticker Modules.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,4 @@
 // list all modules to pack
 
 require('./01/hello.js');
+require('./ticker.js');
---
+++ src/js/ticker.js
@@ -0,0 +1,19 @@
+var Loader = require('./loader.js');
+var Mapper = require('./mapper.js');
+var Renderer = require('./renderer.js');
+
+class Ticker {
+
+    constructor(options) {
+
+    }
+
+    tick() {
+        // load data from URL
+        // parse data
+        // map / aggregate data
+        // create view model
+        // render / display model / update current display
+    }
+
+}
