713260946951e262857fd48fd382875469cb2cdb
aurilein <auriane@gmx.at>
2017-03-22 14:12:01 +0100

    Use webpack script.

--- webpack_production.sh
+++ webpack_production.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Rebuilds assets for production including obfuscation.
-NODE_ENV=production ./node_modules/.bin/webpack $1 $2 $3 $4 $5 $6 $7 $8 $9
+NODE_ENV=production ./webpack.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
